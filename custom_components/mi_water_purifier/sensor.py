8ccb74db1d375c646272066bb0ab35fb5f5ba69d
Devang <devang@hotmail.com>
2023-03-27 11:03:04 +0800

    小米净水器插件，自己动手修改一部分，增加参数 但是缺少水温与状态

--- custom_components/mi_water_purifier/sensor.py
+++ custom_components/mi_water_purifier/sensor.py
@@ -8,16 +8,41 @@ from homeassistant.exceptions import PlatformNotReady
 from miio import Device, DeviceException
 from datetime import timedelta
 
-SCAN_INTERVAL = timedelta(seconds=5)
+SCAN_INTERVAL = timedelta(seconds=10)
 _LOGGER = logging.getLogger(__name__)
 
 TAP_WATER_QUALITY = {'name': 'Tap water', 'key': 'ttds'}
 FILTERED_WATER_QUALITY = {'name': 'Filtered water', 'key': 'ftds'}
 PP_COTTON_FILTER_REMAINING = {'name': 'PP cotton filter', 'key': 'pfd', 'days_key': 'pfp'}
+PP_USED_FLOW = {'name': 'PP filter used flow', 'key': 'ppusedflow'}
+PP_TOTAL_FLOW = {'name': 'PP filter total flow', 'key': 'pptotalflow'}
+PP_USED_TIME = {'name': 'PP filter used time', 'key': 'ppusedtime'}
+PP_TOTAL_TIME = {'name': 'PP filter total time', 'key': 'pptotaltime'}
+PP_REMAINING_TIME = {'name': 'PP filter remaining time', 'key': 'ppremainingtime'}
 FRONT_ACTIVE_CARBON_FILTER_REMAINING = {'name': 'Front active carbon filter', 'key': 'fcfd', 'days_key': 'fcfp'}
+FRONT_USED_FLOW = {'name': 'front filter used flow', 'key': 'frontusedflow'}
+FRONT_TOTAL_FLOW = {'name': 'front filter total flow', 'key': 'fronttotalflow'}
+FRONT_USED_TIME = {'name': 'front filter used time', 'key': 'frontusedtime'}
+FRONT_TOTAL_TIME = {'name': 'front filter total time', 'key': 'fronttotaltime'}
+FRONT_REMAINING_TIME = {'name': 'front filter remaining time', 'key': 'frontremainingtime'}
 RO_FILTER_REMAINING = {'name': 'RO filter', 'key': 'rfd', 'days_key': 'rfp'}
+RO_USED_FLOW = {'name': 'RO filter used flow', 'key': 'rousedflow'}
+RO_TOTAL_FLOW = {'name': 'RO filter total flow', 'key': 'rototalflow'}
+RO_USED_TIME = {'name': 'RO filter used time', 'key': 'rousedtime'}
+RO_TOTAL_TIME = {'name': 'RO filter total time', 'key': 'rototaltime'}
+RO_REMAINING_TIME = {'name': 'RO filter remaining time', 'key': 'roremainingtime'}
 REAR_ACTIVE_CARBON_FILTER_REMAINING = {'name': 'Rear active carbon filter', 'key': 'rcfd', 'days_key': 'rcfp'}
+REAR_USED_FLOW = {'name': 'rear filter used flow', 'key': 'rearusedflow'}
+REAR_TOTAL_FLOW = {'name': 'rear filter total flow', 'key': 'reartotalflow'}
+REAR_USED_TIME = {'name': 'rear filter used time', 'key': 'rearusedtime'}
+REAR_TOTAL_TIME = {'name': 'rear filter total time', 'key': 'reartotaltime'}
+REAR_REMAINING_TIME = {'name': 'rear filter remaining time', 'key': 'rearremainingtime'}
 
+TEST18 = {'name': 'status18', 'key': 'status18'}
+TEST19 = {'name': 'status19', 'key': 'status19'}
+TEST20 = {'name': 'status20', 'key': 'status20'}
+TEST21 = {'name': 'status21', 'key': 'status21'}
+TEST22 = {'name': 'status22', 'key': 'status22'}
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Perform the setup for Xiaomi water purifier."""
@@ -36,9 +61,35 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         devices.append(XiaomiWaterPurifierSensor(waterPurifier, TAP_WATER_QUALITY))
         devices.append(XiaomiWaterPurifierSensor(waterPurifier, FILTERED_WATER_QUALITY))
         devices.append(XiaomiWaterPurifierSensor(waterPurifier, PP_COTTON_FILTER_REMAINING))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, PP_USED_FLOW))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, PP_TOTAL_FLOW))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, PP_USED_TIME))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, PP_TOTAL_TIME))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, PP_REMAINING_TIME))
         devices.append(XiaomiWaterPurifierSensor(waterPurifier, FRONT_ACTIVE_CARBON_FILTER_REMAINING))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, FRONT_USED_FLOW))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, FRONT_TOTAL_FLOW))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, FRONT_USED_TIME))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, FRONT_TOTAL_TIME))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, FRONT_REMAINING_TIME))
         devices.append(XiaomiWaterPurifierSensor(waterPurifier, RO_FILTER_REMAINING))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, RO_USED_FLOW))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, RO_TOTAL_FLOW))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, RO_USED_TIME))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, RO_TOTAL_TIME))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, RO_REMAINING_TIME))
         devices.append(XiaomiWaterPurifierSensor(waterPurifier, REAR_ACTIVE_CARBON_FILTER_REMAINING))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, REAR_USED_FLOW))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, REAR_TOTAL_FLOW))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, REAR_USED_TIME))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, REAR_TOTAL_TIME))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, REAR_REMAINING_TIME))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, TEST18))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, TEST19))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, TEST20))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, TEST21))
+        devices.append(XiaomiWaterPurifierSensor(waterPurifier, TEST22))
+
     except DeviceException:
         _LOGGER.exception('Fail to setup Xiaomi water purifier')
         raise PlatformNotReady
@@ -67,6 +118,28 @@ class XiaomiWaterPurifierSensor(Entity):
         if self._data_key['key'] is TAP_WATER_QUALITY['key'] or \
            self._data_key['key'] is FILTERED_WATER_QUALITY['key']:
             return 'mdi:water'
+        elif self._data_key['key'] is PP_USED_FLOW['key'] or \
+             self._data_key['key'] is PP_TOTAL_FLOW['key'] or \
+             self._data_key['key'] is FRONT_USED_FLOW['key'] or \
+             self._data_key['key'] is FRONT_TOTAL_FLOW['key'] or \
+             self._data_key['key'] is RO_USED_FLOW['key'] or \
+             self._data_key['key'] is RO_TOTAL_FLOW['key'] or \
+             self._data_key['key'] is REAR_USED_FLOW['key'] or \
+             self._data_key['key'] is REAR_TOTAL_FLOW['key']:
+              return 'mdi:water-percent'
+        elif self._data_key['key'] is PP_USED_TIME['key'] or \
+             self._data_key['key'] is PP_TOTAL_TIME['key'] or \
+             self._data_key['key'] is PP_REMAINING_TIME['key'] or \
+             self._data_key['key'] is FRONT_USED_TIME['key'] or \
+             self._data_key['key'] is FRONT_TOTAL_TIME['key'] or \
+             self._data_key['key'] is FRONT_REMAINING_TIME['key'] or \
+             self._data_key['key'] is RO_USED_TIME['key'] or \
+             self._data_key['key'] is RO_TOTAL_TIME['key'] or \
+             self._data_key['key'] is RO_REMAINING_TIME['key'] or \
+             self._data_key['key'] is REAR_USED_TIME['key'] or \
+             self._data_key['key'] is REAR_TOTAL_TIME['key'] or \
+             self._data_key['key'] is REAR_REMAINING_TIME['key']:
+              return 'mdi:clock'
         else:
             return 'mdi:filter-outline'
 
@@ -81,6 +154,30 @@ class XiaomiWaterPurifierSensor(Entity):
         if self._data_key['key'] is TAP_WATER_QUALITY['key'] or \
            self._data_key['key'] is FILTERED_WATER_QUALITY['key']:
             return 'TDS'
+        elif self._data_key['key'] is PP_USED_FLOW['key'] or \
+             self._data_key['key'] is PP_TOTAL_FLOW['key'] or \
+             self._data_key['key'] is FRONT_USED_FLOW['key'] or \
+             self._data_key['key'] is FRONT_TOTAL_FLOW['key'] or \
+             self._data_key['key'] is RO_USED_FLOW['key'] or \
+             self._data_key['key'] is RO_TOTAL_FLOW['key'] or \
+             self._data_key['key'] is REAR_USED_FLOW['key'] or \
+             self._data_key['key'] is REAR_TOTAL_FLOW['key']:
+              return 'L'
+        elif self._data_key['key'] is PP_USED_TIME['key'] or \
+             self._data_key['key'] is PP_TOTAL_TIME['key'] or \
+             self._data_key['key'] is PP_REMAINING_TIME['key'] or \
+             self._data_key['key'] is FRONT_USED_TIME['key'] or \
+             self._data_key['key'] is FRONT_TOTAL_TIME['key'] or \
+             self._data_key['key'] is FRONT_REMAINING_TIME['key'] or \
+             self._data_key['key'] is RO_USED_TIME['key'] or \
+             self._data_key['key'] is RO_TOTAL_TIME['key'] or \
+             self._data_key['key'] is RO_REMAINING_TIME['key'] or \
+             self._data_key['key'] is REAR_USED_TIME['key'] or \
+             self._data_key['key'] is REAR_TOTAL_TIME['key'] or \
+             self._data_key['key'] is REAR_REMAINING_TIME['key']:
+              return 'days'
+        else:
+            return '%'
         return '%'
 
     @property
@@ -146,9 +243,35 @@ class XiaomiWaterPurifier(Entity):
         attrs = {}
         attrs[TAP_WATER_QUALITY['name']] = '{}TDS'.format(self._data[TAP_WATER_QUALITY['key']])
         attrs[PP_COTTON_FILTER_REMAINING['name']] = '{}%'.format(self._data[PP_COTTON_FILTER_REMAINING['key']])
+        attrs[PP_USED_FLOW['name']] = '{}L'.format(self._data[PP_USED_FLOW['key']])
+        attrs[PP_TOTAL_FLOW['name']] = '{}L'.format(self._data[PP_TOTAL_FLOW['key']])
+        attrs[PP_USED_TIME['name']] = '{}days'.format(self._data[PP_USED_TIME['key']])
+        attrs[PP_TOTAL_TIME['name']] = '{}days'.format(self._data[PP_TOTAL_TIME['key']])
+        attrs[PP_REMAINING_TIME['name']] = '{}days'.format(self._data[PP_REMAINING_TIME['key']])
         attrs[FRONT_ACTIVE_CARBON_FILTER_REMAINING['name']] = '{}%'.format(self._data[FRONT_ACTIVE_CARBON_FILTER_REMAINING['key']])
+        attrs[FRONT_USED_FLOW['name']] = '{}L'.format(self._data[FRONT_USED_FLOW['key']])
+        attrs[FRONT_TOTAL_FLOW['name']] = '{}L'.format(self._data[FRONT_TOTAL_FLOW['key']])
+        attrs[FRONT_USED_TIME['name']] = '{}days'.format(self._data[FRONT_USED_TIME['key']])
+        attrs[FRONT_TOTAL_TIME['name']] = '{}days'.format(self._data[FRONT_TOTAL_TIME['key']])
+        attrs[FRONT_REMAINING_TIME['name']] = '{}days'.format(self._data[FRONT_REMAINING_TIME['key']])
         attrs[RO_FILTER_REMAINING['name']] = '{}%'.format(self._data[RO_FILTER_REMAINING['key']])
+        attrs[RO_USED_FLOW['name']] = '{}L'.format(self._data[RO_USED_FLOW['key']])
+        attrs[RO_TOTAL_FLOW['name']] = '{}L'.format(self._data[RO_TOTAL_FLOW['key']])
+        attrs[RO_USED_TIME['name']] = '{}days'.format(self._data[RO_USED_TIME['key']])
+        attrs[RO_TOTAL_TIME['name']] = '{}days'.format(self._data[RO_TOTAL_TIME['key']])
+        attrs[RO_REMAINING_TIME['name']] = '{}days'.format(self._data[RO_REMAINING_TIME['key']])
         attrs[REAR_ACTIVE_CARBON_FILTER_REMAINING['name']] = '{}%'.format(self._data[REAR_ACTIVE_CARBON_FILTER_REMAINING['key']])
+        attrs[REAR_USED_FLOW['name']] = '{}L'.format(self._data[REAR_USED_FLOW['key']])
+        attrs[REAR_TOTAL_FLOW['name']] = '{}L'.format(self._data[REAR_TOTAL_FLOW['key']])
+        attrs[REAR_USED_TIME['name']] = '{}days'.format(self._data[REAR_USED_TIME['key']])
+        attrs[REAR_TOTAL_TIME['name']] = '{}days'.format(self._data[REAR_TOTAL_TIME['key']])
+        attrs[REAR_REMAINING_TIME['name']] = '{}days'.format(self._data[REAR_REMAINING_TIME['key']])
+
+        attrs[TEST18['name']] = '{} test18'.format(self._data[TEST18['key']])
+        attrs[TEST19['name']] = '{} test19'.format(self._data[TEST19['key']])
+        attrs[TEST20['name']] = '{} test20'.format(self._data[TEST20['key']])
+        attrs[TEST21['name']] = '{} test21'.format(self._data[TEST21['key']])
+        attrs[TEST22['name']] = '{} test22'.format(self._data[TEST22['key']])
 
         return attrs
 
@@ -162,15 +285,53 @@ class XiaomiWaterPurifier(Entity):
             pfd = int((status[11] - status[3]) / 24)
             data[PP_COTTON_FILTER_REMAINING['days_key']] = pfd
             data[PP_COTTON_FILTER_REMAINING['key']] = math.floor(pfd * 24 * 100 / status[11])
+            data[PP_USED_FLOW['key']] = status[2]
+            data[PP_TOTAL_FLOW['key']] = status[10]
+            ppusedtime = int(status[3]/24)
+            data[PP_USED_TIME['key']] = ppusedtime
+            pptotaltime = int(status[11]/24)
+            data[PP_TOTAL_TIME['key']] = pptotaltime
+            ppremainingtime = int((status[11] - status[3])/24)
+            data[PP_REMAINING_TIME['key']] = ppremainingtime
             fcfd = int((status[13] - status[5]) / 24)
             data[FRONT_ACTIVE_CARBON_FILTER_REMAINING['days_key']] = fcfd
             data[FRONT_ACTIVE_CARBON_FILTER_REMAINING['key']] = math.floor(fcfd * 24 * 100 / status[13])
+            data[FRONT_USED_FLOW['key']] = status[4]
+            data[FRONT_TOTAL_FLOW['key']] = status[12]
+            frontusedtime = int(status[5]/24)
+            data[FRONT_USED_TIME['key']] = frontusedtime
+            fronttotaltime = int(status[13]/24)
+            data[FRONT_TOTAL_TIME['key']] = fronttotaltime
+            frontremainingtime = int((status[13] - status[5])/24)
+            data[FRONT_REMAINING_TIME['key']] = frontremainingtime
             rfd = int((status[15] - status[7]) / 24)
             data[RO_FILTER_REMAINING['days_key']] = rfd
             data[RO_FILTER_REMAINING['key']] = math.floor(rfd * 24 * 100 / status[15])
+            data[RO_USED_FLOW['key']] = status[6]
+            data[RO_TOTAL_FLOW['key']] = status[14]
+            rousedtime = int(status[7]/24)
+            data[RO_USED_TIME['key']] = rousedtime
+            rototaltime = int(status[15]/24)
+            data[RO_TOTAL_TIME['key']] = rototaltime
+            roremainingtime = int((status[15] - status[7])/24)
+            data[RO_REMAINING_TIME['key']] = roremainingtime
             rcfd = int((status[17] - status[9]) / 24)
             data[REAR_ACTIVE_CARBON_FILTER_REMAINING['days_key']] = rcfd
             data[REAR_ACTIVE_CARBON_FILTER_REMAINING['key']] = math.floor(rcfd * 24 * 100 / status[17])
+            data[REAR_USED_FLOW['key']] = status[8]
+            data[REAR_TOTAL_FLOW['key']] = status[16]
+            rearusedtime = int(status[9]/24)
+            data[REAR_USED_TIME['key']] = rearusedtime
+            reartotaltime = int(status[17]/24)
+            data[REAR_TOTAL_TIME['key']] = reartotaltime
+            rearremainingtime = int((status[17] - status[9])/24)
+            data[REAR_REMAINING_TIME['key']] = rearremainingtime
+
+            data[TEST18['key']] = status[18]
+            data[TEST19['key']] = status[19]
+            data[TEST20['key']] = status[20]
+            data[TEST21['key']] = status[21]
+            data[TEST22['key']] = status[22]
 
             self._data = data
             self._state = self._data[FILTERED_WATER_QUALITY['key']]
